9490d58a22b033a420b054ece13b8692467fd616
hjh <98207658+jjjuhoon@users.noreply.github.com>
2024-08-02 07:02:42 +0900

    모델 응답,

--- src/main/java/Helloworld/helloworld_webflux/web/dto/GPTRequest.java
+++ src/main/java/Helloworld/helloworld_webflux/web/dto/GPTRequest.java
@@ -1,20 +1,22 @@
 package Helloworld.helloworld_webflux.web.dto;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
+import java.util.Objects;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class GPTRequest {
     private String model;
-    private List<Message> messages;
+    private List<Object> messages;
     private int max_tokens;
 
-    public GPTRequest(List<Message> messages) {
+    public GPTRequest(List<Object> messages) {
         this.model = "gpt-3.5-turbo";  // 모델. gpt4가 더 좋으려나? - 의논
         this.messages = messages;
         this.max_tokens = 1000;  // 필요한 경우 토큰 수 조정
@@ -26,5 +28,13 @@ public class GPTRequest {
     public static class Message {
         private String role;
         private String content;
+    }
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class MessageJson {
+        //        private String role;
+//        private String content;
+        JsonNode contentJson;
     }
 }
