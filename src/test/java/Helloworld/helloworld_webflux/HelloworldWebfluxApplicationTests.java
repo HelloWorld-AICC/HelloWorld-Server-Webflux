998c80448abbe4851b34b22695e45ff53c822482
hjh <ghkdwngns@naver.com>
2024-08-27 14:34:00 +0900

    test삭제

--- src/test/java/Helloworld/helloworld_webflux/HelloworldWebfluxApplicationTests.java
+++ src/test/java/Helloworld/helloworld_webflux/HelloworldWebfluxApplicationTests.java
@@ -1,13 +1,13 @@
-package Helloworld.helloworld_webflux;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class HelloworldWebfluxApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package Helloworld.helloworld_webflux;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class HelloworldWebfluxApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
