3ef9a47cfc6afdd60a24f3883eaf03103bc905d7
hjh <ghkdwngns@naver.com>
2024-10-07 23:17:11 +0900

    대화방 로그 조회

--- src/main/java/Helloworld/helloworld_webflux/service/RoomService.java
+++ src/main/java/Helloworld/helloworld_webflux/service/RoomService.java
@@ -1,8 +1,11 @@
 package Helloworld.helloworld_webflux.service;
 
+import Helloworld.helloworld_webflux.web.dto.RecentRoomDTO;
 import Helloworld.helloworld_webflux.web.dto.RoomDTO;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface RoomService {
     Flux<RoomDTO> getUserRooms(Long userId);
+    Mono<RecentRoomDTO> findRoomLogs(String roomId);
 }
