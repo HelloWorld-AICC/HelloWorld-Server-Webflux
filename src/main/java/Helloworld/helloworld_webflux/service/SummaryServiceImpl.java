d293ce441f37fa44d4ab20643e3c8244a3694327
hjh <ghkdwngns@naver.com>
2025-11-08 00:25:22 +0900

    chat api 수정

--- src/main/java/Helloworld/helloworld_webflux/service/SummaryServiceImpl.java
+++ src/main/java/Helloworld/helloworld_webflux/service/SummaryServiceImpl.java
@@ -31,40 +31,41 @@ public class SummaryServiceImpl implements SummaryService {
 
     @Override
     public Mono<String> generateSummary(String gmail, String roomId) {
-        return userRepository.findByEmail(gmail).flatMap(user -> {
-            return translateLogRepository.findByRoomIdOrderByTimeAsc(roomId)
-                    .collectList()
-                    .flatMap(messages -> {
-                        String concatenatedLogs = messages.stream()
-                                .map(TranslateLog::getContent)
-                                .reduce((acc, log) -> acc + "\n" + log)
-                                .orElse("");
-
-                        return createSummary(concatenatedLogs);
-                    })
-                    .flatMap(summaryDTO -> userService.findLanguage(gmail)
-                            .flatMap(language -> translateToUserLanguage(summaryDTO.getChatSummary(), language)
-                                    .flatMap(translatedSummary -> {
-                                        Summary summary = Summary.builder()
-                                                .identificationNum(generateIdentificationNum())
-                                                .status("EMPTY")
-                                                .title(summaryDTO.getTitle())
-                                                .chatSummary(translatedSummary)
-                                                .mainPoint(summaryDTO.getMainPoint())
-                                                .userId(user.getId()) // userId 대신 user 객체를 사용
-                                                .createdAt(LocalDateTime.now())
-                                                .updatedAt(LocalDateTime.now())
-                                                .roomId(roomId)
-                                                .build();
-                                        return summaryRepository.save(summary);
-                                    }))
-                    )
-                    .then(Mono.just("complete"));
-        });
+        return userRepository.findByEmail(gmail).flatMap(user ->
+                userService.findLanguage(gmail).flatMap(language ->
+                        translateLogRepository.findByRoomIdOrderByTimeAsc(roomId)
+                                .collectList()
+                                .flatMap(messages -> {
+                                    String concatenatedLogs = messages.stream()
+                                            .map(TranslateLog::getContent)
+                                            .reduce((acc, log) -> acc + "\n" + log)
+                                            .orElse("");
+                                    return createSummary(concatenatedLogs, language);
+                                })
+                                .flatMap(summaryDTO -> {
+                                    Summary summary = Summary.builder()
+                                            .identificationNum(generateIdentificationNum())
+                                            .status("EMPTY")
+                                            .title(summaryDTO.getTitle())
+                                            .chatSummary(summaryDTO.getChatSummary())
+                                            .mainPoint(summaryDTO.getMainPoint())
+                                            .userId(user.getId())
+                                            .createdAt(LocalDateTime.now())
+                                            .updatedAt(LocalDateTime.now())
+                                            .roomId(roomId)
+                                            .build();
+                                    return summaryRepository.save(summary);
+                                })
+                                .then(Mono.just("complete"))
+                )
+        );
     }
 
-    private Mono<SummaryDTO> createSummary(String logs) {
-        GPTRequest.Message systemMessage = new GPTRequest.Message("system", "You are a helpful assistant that summarizes chat logs.");
+    private Mono<SummaryDTO> createSummary(String logs, String targetLanguage) {
+        GPTRequest.Message systemMessage = new GPTRequest.Message(
+                "system",
+                String.format("You are a helpful assistant that summarizes chat logs in %s.", targetLanguage)
+        );
         GPTRequest.Message userMessage = new GPTRequest.Message("user",
                 "Summarize the following conversation with a focus on clarity and conciseness. " +
                         "Provide the response in three distinct sections: " +
@@ -81,10 +82,17 @@ public class SummaryServiceImpl implements SummaryService {
                         "Title:  [Title]\n" +
                         "Chat Summary:  [Summary]\n" +
                         "Main Points:  [Main Points]\n\n" +
-                        "Here is example:\n" +
+                        "Here is example (for format only, not for tone or style):\n" +
                         "Title:  How to get E-9 visa\n" +
                         "Chat Summary:  The user, who is from *Australia*, inquires about obtaining an *E-9 visa* for South Korea. The AI assistant explains that the E-9 visa is for *foreign workers* engaging in *non-professional employment* in South Korea and outlines the necessary procedures and requirements. The user must meet specific criteria, including a *job offer* from a Korean employer and passing *medical and background checks*.\n" +
-                        "Main Points:  1. *E-9 visa* is for non-professional employment in South Korea. 2. The user needs a *job offer* from a Korean employer. 3. *Medical and background checks* are required for visa approval."
+                        "Main Points:  1. *E-9 visa* is for non-professional employment in South Korea. 2. The user needs a *job offer* from a Korean employer. 3. *Medical and background checks* are required for visa approval.\n" +
+                        "\nFocus only on summarizing the useful information or solution discussed, not describing who asked or answered."+
+
+                        "You Must Respond entirely in " + targetLanguage + " using natural and fluent expressions. This is very Super Important. " +
+                        "Do NOT describe who asked or answered questions. " +
+                        "Instead, provide the key information or knowledge that can be learned from this conversation as if it were a guide or explanation. " +
+                        "Focus on summarizing the *content* and *information*, not the dialogue itself."
+
         );
 
 
@@ -122,22 +130,22 @@ public class SummaryServiceImpl implements SummaryService {
     }
 
 
-    private Mono<String> translateToUserLanguage(String text, String targetLanguage) {
-        GPTRequest.Message systemMessage = new GPTRequest.Message("system", "You are a great translator.");
-        GPTRequest.Message userMessage = new GPTRequest.Message("user",
-                "Translate the following text to " + targetLanguage +
-                        " exactly. Keep *Markdown bold* formatting as it is: " + text);
-        GPTRequest request = new GPTRequest("gpt-3.5-turbo", List.of(systemMessage, userMessage), 3000);
-
-        return webClient.post()
-                .uri("https://api.openai.com/v1/chat/completions")
-                .header("Authorization", "Bearer " + openaiApiKey)
-                .header("Content-Type", "application/json")
-                .bodyValue(request)
-                .retrieve()
-                .bodyToMono(GPTResponse.class)
-                .map(response -> response.getChoices().get(0).getMessage().getContent());
-    }
+//    private Mono<String> translateToUserLanguage(String text, String targetLanguage) {
+//        GPTRequest.Message systemMessage = new GPTRequest.Message("system", "You are a great translator.");
+//        GPTRequest.Message userMessage = new GPTRequest.Message("user",
+//                "Translate the following text to " + targetLanguage +
+//                        " exactly. Keep *Markdown bold* formatting as it is: " + text);
+//        GPTRequest request = new GPTRequest("gpt-3.5-turbo", List.of(systemMessage, userMessage), 3000);
+//
+//        return webClient.post()
+//                .uri("https://api.openai.com/v1/chat/completions")
+//                .header("Authorization", "Bearer " + openaiApiKey)
+//                .header("Content-Type", "application/json")
+//                .bodyValue(request)
+//                .retrieve()
+//                .bodyToMono(GPTResponse.class)
+//                .map(response -> response.getChoices().get(0).getMessage().getContent());
+//    }
 
     private String generateIdentificationNum() {
         return "ID-" + LocalDateTime.now().toString();
